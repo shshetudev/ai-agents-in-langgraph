60a7ac00a73e1833ea6d3b6829ba44744ecb412b
Md. Shahariar Hossen <shshetudev@gmail.com>
2026-01-08 12:37:34 +0600

    feat(solid-principles): Open/Closed Principle

---
+++ python-oop/solid/ocp.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+import math
+
+class Shape(ABC):
+    @abstractmethod
+    def calculate_area(self) -> float:
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius: float):
+        self._radius = radius
+
+    def calculate_area(self) -> float:
+        return math.pi * (self._radius ** 2)
+
+class Rectangle(Shape):
+    def __init__(self, width: float, height: float):
+        self._width = width
+        self._height = height
+
+    def calculate_area(self) -> float:
+        return self._width * self._height
+
+if __name__  == "__main__":
+    circle = Circle(10)
+    rectangle = Rectangle(4, 5)
+
+    print(f"Circle area: {circle.calculate_area()}")
+    print(f"Rectangle area: {rectangle.calculate_area()}")
